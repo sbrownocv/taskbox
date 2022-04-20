b36c6185f851eea4b1c495c45d4f7f5846d82d9d
Sid <sbrown@myocv.com>
2022-04-20 09:41:16 -0500

    Github action setup #2

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -17,7 +17,7 @@ export default function InboxScreen() {
       <div className="page lists-show">
         <div className="wrapper-message">
           <span className="icon-face-sad" />
-          <div className="title-message">Oh no!</div>
+          <div className="title-message">Oh NOOO!</div>
           <div className="subtitle-message">Something went wrong</div>
         </div>
       </div>
